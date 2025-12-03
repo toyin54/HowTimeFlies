098e7d84a091c9382d4069612c7ea228e84c8136
toyin <toyin54@live.com>
2025-12-02 19:40:29 -0600

    Initial Commit

---
+++ HowTimeFlies/HowTimeFliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowTimeFliesApp.swift
+//  HowTimeFlies
+//
+//  Created by Ayyub Jose on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HowTimeFliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
